476a6b6f0f93cab8a628769514c1cff33a1ea911
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-10 01:19:34 +0900

    feat: 햄버거메뉴 로그인 로그아웃 추가

---
+++ client/components/layouts/HamburgerLogin.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+<template>
+    <div class="greeting-container">
+        <p class="greeting">안녕하세요.<br>대문 님</p>
+        <ul class="menu-list">
+          <li><a href="#" class="cart"><q-icon size="18px" name="shop"/></a></li>
+          <li><a href="#" class="settings"><q-icon size="18px" name="settings"/></a></li>
+          <li><a href="#" class="logout"><q-icon size="18px" name="logout"/></a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.greeting-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: #f5f5f5;
+}
+
+.greeting {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.menu-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+
+    li {
+        margin-left: 15px;
+    }
+
+    a {
+        text-decoration: none;
+        color: #333;
+        &:hover {
+            color: #007bff;
+        }
+    }
+}
+</style>
---
+++ client/components/layouts/HamburgerLogout.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+<template>
+    <div class="button-container">
+        <nuxt-link to="/auth/login" class="login-button">로그인</nuxt-link>
+        <nuxt-link to="/auth/join" class="signup-button">회원가입</nuxt-link>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.button-container {
+    display: flex;
+    justify-content: center;
+    gap: 10px; // 버튼 간격
+    padding: 20px;
+    background-color: #f5f5f5;
+}
+
+.login-button,.signup-button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: #007bff;
+        color: white;
+    }
+}
+
+.login-button {
+    background-color: #007bff;
+    color: white;
+}
+
+.signup-button {
+    background-color: #acadf3;
+    color: white;
+}
+</style>
