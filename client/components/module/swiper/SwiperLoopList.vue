0b9692a582a1fa7d626c8098375c80f524b5501b
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-10 01:16:30 +0900

    feat: swiper loop 조건 추가

--- client/components/module/swiper/SwiperLoopList.vue
+++ client/components/module/swiper/SwiperLoopList.vue
@@ -1,53 +1,63 @@
-<script lang="ts" setup>
+<script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Autoplay, Controller } from 'swiper';
 import SwiperControls from "~/components/module/swiper/SwiperControls.vue";
 import 'swiper/css';
 const modules = [Autoplay, Controller];
 const visualSwiper = ref(null);
+const isAutoplayPaused = ref(false);
 const swiperCard = defineModel('swiperCard',{
     type: Array
 })
+const showSwiperCnt = defineModel('showSwiperCnt',{
+    type: Number
+})
+
 const onSwiper = (swiperInstance) => {
     visualSwiper.value = swiperInstance;
+    visualSwiper.value.isAutoplayPaused = isAutoplayPaused.value;
 };
 // 마우스 호버 시 자동 재생 일시 중지
 const onMouseEnter = () => {
     if (visualSwiper.value && visualSwiper.value.autoplay) {
         visualSwiper.value.autoplay.stop();
+        visualSwiper.value.isAutoplayPaused = true
     }
 };
-
 // 마우스 이탈 시 자동 재생 재개
 const onMouseLeave = () => {
     if (visualSwiper.value && visualSwiper.value.autoplay) {
         visualSwiper.value.autoplay.start();
+        visualSwiper.value.isAutoplayPaused = false
     }
 };
 </script>
 <template>
     <Swiper
-      :slides-per-view="4"
+      :slides-per-view="showSwiperCnt"
       :loop="true"
       :autoplay="{
-        delay: 1000,
-        disableOnInteraction: true,
+        delay: 4000
       }"
       :modules="modules"
+      :class="showSwiperCnt === 1 ?'parent-div-relative':''"
       @swiper="onSwiper"
-      @mouseenter="onMouseEnter" 
-      @mouseleave="onMouseLeave"
     >
-      <SwiperSlide v-for="(slide, index) in swiperCard" :key="index">
+      <SwiperSlide v-for="(slide, index) in swiperCard" :key="index"  @mouseenter="showSwiperCnt > 1 && onMouseEnter" @mouseleave="showSwiperCnt > 1 && onMouseLeave">
         <!-- {{slide}} -->
           <slot name="item"/>
       </SwiperSlide>
-      <SwiperControls :swiper="visualSwiper" />
+      <SwiperControls v-model:visualSwiper="visualSwiper" :class="showSwiperCnt < 2 ?'swiper-controls':''" class="row justify-end" />
     </Swiper>
 </template>
-<style>
-.content {
-  background-color: #ddd;
-  text-align: center;
+<style lang="scss" scoped>
+.parent-div-relative {
+  position: relative;
+}
+.swiper-controls {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  z-index: inherit;
 }
 </style>
