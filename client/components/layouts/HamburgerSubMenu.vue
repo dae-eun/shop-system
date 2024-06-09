e6c0a29ecd37c20a58f8a08b736acdb688ecac0e
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-10 01:14:47 +0900

    feat: submenu 이름변경 및 구조생성

---
+++ client/components/layouts/HamburgerSubMenu.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+      <q-list bordered>
+            <q-expansion-item
+                label="가나다"
+                class="active"
+            >
+              <q-card>
+                <q-card-section>
+                  123
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+            <q-item>
+                <nuxt-link>이동</nuxt-link>
+            </q-item>
+      </q-list>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.active {
+  background: #3769e7;
+  color: white;
+}
+</style>
