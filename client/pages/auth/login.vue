c59e894ebcefcf1bb9cd4a79b203c94bce577aef
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-10 23:21:24 +0900

    feat: 로그인 구현

--- client/pages/auth/login.vue
+++ client/pages/auth/login.vue
@@ -1,13 +1,30 @@
 <script setup>
-  const user = ref({
-    email: '',
-    password: '',
-  });
-  
-  const login = async () => {
-    // TODO send user Data to the login endpoint and redirect if  successful 
-    console.log(123)
-  };
+import { useAuthStore } from "~/stores/auth/loginStore";
+const { $event } = useNuxtApp();
+
+const user = ref({
+  email: "",
+  password: "",
+});
+
+const checkVali=()=>{
+  if(!user.value.email) {
+    $event("onAlertModal", { contentVal: "아이디를 입력해주세요." });
+    return false
+  }
+  if(!user.value.password) {
+    $event("onAlertModal", { contentVal: "비밀번호를 입력해주세요." });
+    return false
+  }
+  return true
+}
+
+const login = async () => {
+  if(!checkVali()) return
+  await useAuthStore().login(user.value);
+  if(useAuthStore().status==='error') return $event("onAlertModal", { contentVal: '로그인에 실패했습니다.' });
+  navigateTo('/')
+};
 </script>
 <template>
   <div class="flex mT60 justify-center items-center h-screen bg-gray-200">
@@ -15,8 +32,20 @@
       <q-card-section class="login-ipt">
         <div class="text-h6 text-center q-mb-md">로그인</div>
         <q-form @submit="login">
-          <q-input v-model="user.email" class="mT10" outlined label="이메일" autofocus />
-          <q-input v-model="user.password" class="mT10" outlined label="비밀번호" type="password" />
+          <q-input
+            v-model="user.email"
+            class="mT10"
+            outlined
+            label="이메일"
+            autofocus
+          />
+          <q-input
+            v-model="user.password"
+            class="mT10"
+            outlined
+            label="비밀번호"
+            type="password"
+          />
           <div class="btn-center">
             <q-btn label="로그인" type="submit" color="primary" />
           </div>
@@ -30,19 +59,23 @@
 .w-600 {
   width: 600px;
 }
-.login-ipt{
+
+.login-ipt {
   display: flex;
   flex-direction: column;
   padding: 5.4rem 4.4rem 3rem;
+
   .q-btn {
-      font-size: 2rem;
+    font-size: 2rem;
+    border-radius: 5px;
+    height: 7rem;
+    margin-top: 1.5rem;
+
+    &:before {
       border-radius: 5px;
-      height: 7rem;
-      margin-top: 1.5rem;
-      &:before {
-        border-radius: 5px;
-      }
     }
+  }
+
   .btn-center {
     display: flex;
     justify-content: center;
