66ce7ca3870fe5853c8401544df9203a400398f7
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-11 16:02:13 +0900

    feat: 린트적용

--- client/composables/utils/addressSearch.js
+++ client/composables/utils/addressSearch.js
@@ -1,9 +1,9 @@
 export const addressSearch = (callback) => {
-  const script = document.createElement("script");
-  script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+  const script = document.createElement('script');
+  script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
   script.onload = function () {
     window.daumPostcode = new window.daum.Postcode({
-      oncomplete: function (data) {
+      oncomplete(data) {
         callback(data);
       },
     }).open();
