6457270f8d27bfb4f7af2dea6335013bd43d3fd5
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-18 04:19:04 +0900

    feat: 사용자 메뉴 조회 api 및 store 생성

---
+++ client/server/api/user/menu.get.ts
@@ -0,0 +1,24 @@
+import { serverSupabaseClient, serverSupabaseUser } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+  const user = await serverSupabaseUser(event);
+  const client = await serverSupabaseClient(event);
+
+  if (!user) return { statusCode: 401, message: 'Authentication required' };
+
+  // 전체 메뉴를 조회
+  const { data, error } = await client
+    .from('TB_MENU')
+    .select('*')
+    .filter('useAt', 'eq', true)
+    .order('sortOrdr');
+
+  if (error) {
+    console.error('Error fetching menu:', error);
+    return { statusCode: 500, message: 'Internal Server Error' };
+  } else if (!data) {
+    return { statusCode: 404, message: '메뉴가 존재하지 않습니다.' };
+  } else {
+    return { statusCode: 200, message: '조회하였습니다.', menuInfo: data };
+  }
+});
---
+++ client/stores/user/menuStore.ts
@@ -0,0 +1,89 @@
+import { defineStore } from 'pinia';
+import { useFetchBase } from '~/composables/api/index';
+
+interface Menu {
+  children?: Menu[]
+  label?: string
+  menuId: number
+  menuNm: string
+  upperMenuId: number
+  level: number
+  url: string
+  menuType: string
+  pageType: string
+  boardType: string
+  sortOrdr: number
+  useAt: boolean
+  header?: string
+}
+
+interface MenuResponse {
+  statusCode: number
+  message: string
+  menuInfo?: Menu[]
+}
+
+interface SystemError {
+  code: string
+  message: string
+}
+
+export const controllMenuStore = defineStore('controllMenuStore', {
+  state: () => ({
+    menuInfo: [] as Menu[],
+    statusCode: null as number | null,
+    message: '',
+    error: '',
+  }),
+  actions: {
+    async getData() {
+      try {
+        const { statusCode, message, menuInfo } = await useFetchBase('/api/user/menu', {
+          method: 'GET',
+        }) as MenuResponse;
+        this.statusCode = statusCode;
+        this.message = message;
+        if (statusCode === 200) {
+          this.menuInfo = menuInfo || [];
+          this.menuInfo.map((el) => {
+            el.label = el.menuNm;
+            el.header = el.menuType;
+          });
+        }
+        return this.$state;
+      } catch (error) {
+        const err = error as SystemError;
+        this.error = err.message;
+      }
+    },
+  },
+  getters: {
+    getMenu(state) {
+      return state.menuInfo;
+    },
+    getMenuTree(state) {
+      const tree: Menu[] = [];
+      const lookup: { [key: number]: Menu } = {};
+
+      // 각 메뉴 항목을 초기화하여 lookup 객체에 저장
+      state.menuInfo.forEach((menu) => {
+        lookup[menu.menuId] = { ...menu };
+      });
+
+      // 트리 구조 형성
+      state.menuInfo.forEach((menu) => {
+        if (menu.upperMenuId) {
+          const upperItem = lookup[menu.upperMenuId];
+          if (!upperItem.children) {
+            upperItem.children = [];
+          }
+          upperItem.children.push(lookup[menu.menuId]);
+        } else {
+          tree.push(lookup[menu.menuId]);
+        }
+      });
+
+      return tree;
+    },
+  },
+});
