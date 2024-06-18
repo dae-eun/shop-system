63e84fc9e6bb3f93d37e9c19325e28b718dd71d8
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-18 17:14:30 +0900

    feat: 미들웨어 404 추가 및 순서 추가

--- client/middleware/2.authCheck.global.ts
+++ client/middleware/2.authCheck.global.ts
@@ -4,6 +4,8 @@ export default defineNuxtRouteMiddleware(async () => {
   const user = useSupabaseUser();
   const authStore = getUserInfoStore();
   authStore.isMiddlewareLoaded = false;
+  console.log(!user.value);
+  console.log(!authStore.isLogin);
   if (!user.value) return authStore.isMiddlewareLoaded = true;
   if (!authStore.isLogin) {
     await authStore.getUserInfo();
