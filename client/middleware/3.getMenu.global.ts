d4dd71cf94b7f695a75bb6daeb5266d0657e84f6
dae-eun <111928719+dae-eun@users.noreply.github.com>
2024-06-21 18:58:34 +0900

    feat: 클라이언트 미들웨어 조건추가

--- client/middleware/3.getMenu.global.ts
+++ client/middleware/3.getMenu.global.ts
@@ -2,7 +2,9 @@ import { getUserMenuStore } from '~/stores/user/userMenuStore';
 
 export default defineNuxtRouteMiddleware(async () => {
   try {
-    await getUserMenuStore().getData();
+    if (!getUserMenuStore().menuInfo.length) {
+      await getUserMenuStore().getData();
+    }
   } catch (error) {
     console.error('Failed to get user menu:', error);
   }
